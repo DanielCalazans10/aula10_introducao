9d6fa0237abb3a4e308fb99db1d438a7e7fb1424
DanielCalazans10 <danielcalazans98@hotmail.com>
2025-03-10 14:27:44 -0300

    versão base do código

---
+++ app/contato/page.js
@@ -0,0 +1,56 @@
+'use client'
+import { useState } from "react"
+import { ToastContainer } from "react-toastify"
+
+export default function Contato(){
+
+    const [ nome, alteraNome ] = useState("")
+    const [mensagem, alteraMensagem ] = useState("")
+    const notify = () => toast ("Mensagem Enviada");
+ 
+    function toastify(e){
+
+    }
+    function enviaMensagem(e){
+
+        e.preventDefault()
+
+
+
+        if( nome == "" || mensagem == ""){
+            
+     }else{
+        
+     }
+    
+
+    }
+
+    return(
+         <div>
+              <h1>Página de contato {nome}</h1>
+              <p>Entre em contato conosco.</p>
+
+              <hr/>
+
+              <form onSubmit={ (e)=> enviaMensagem(e) }>
+                <label>
+                    Digite seu nome:
+                    <input onChange={ (e)=> alteraNome(e.target.value) }/>
+                </label>
+                <br/>
+                <label>
+                    Digite sua mensagem:
+                    <input onChange={ (e)=> alteraMensagem(e.target.value)}/>
+                </label>
+
+                <br/>
+
+                <button onClick={notify}>Enviar</button>
+                <ToastContainer />
+                
+              </form>
+
+           </div>)
+
+}
